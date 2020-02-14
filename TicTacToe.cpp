0e2eb02c963369a936835704d05608afe3ccec6a
Danny Nguyen <dang0781@colorado.edu>
2020-02-14 16:38:36 -0700

    Added clarification and removed code to display board for now

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,21 +2,12 @@
 
 class TicTacToe {
     public:
-        TicTacToe() {
-            for (int i = 0; i < 9; i++) {
-                arr_[i / 3][i % 3] = '1' + i;
-            }
-        }
         void CreateBoard() {
-
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    std::cout << arr_[i][j] << " ";
-                }
-                std::cout << std::endl;
+            for (int i = 0; i < 9; i++) { // in matrix of 9 values give each square a unique value to assign an x or o to.
+                arr_[i / 3][i % 3] = '1' + i; // adding char to int type casts the char to an int resulting in no type error
             }
         }
-
+        
     private:
         char arr_[3][3];
 };
